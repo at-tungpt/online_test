92ca7f3a5ec2672201ba47a3ae8c158a6b1e4f63
at-tungpt <tungpt@asiantech.vn>
2017-01-18 17:09:21 +0700

    fix feature#5

--- resources/views/admin/teacher/table.blade.php
+++ resources/views/admin/teacher/table.blade.php
@@ -1,11 +1,11 @@
 <table>
     <thead>
-        <th>No.</th>
-        <th>Fullname</th>
-        <th>Email</th>
-        <th>Birthday</th>
-        <th>Avatar<th>
-        <th>Action</th>
+        <th>{{ trans('label_trans.no')}}</th>
+        <th>{{ trans('label_trans.fullname')}}</th>
+        <th>{{ trans('label_trans.email')}}</th>
+        <th>{{ trans('label_trans.birthday')}}</th>
+        <th>{{ trans('label_trans.avatar')}}<th>
+        <th>{{ trans('label_trans.action')}}</th>
     </thead>
     <tbody>
     @foreach($user as $user)
@@ -14,13 +14,11 @@
         <td>{{ $user->name }}</td>
         <td class="email">{{ $user->email }}</td>
         <td>{{ $user->birthday }}</td>
-        <td>{{ $user->image}}</td>
+        <td>{{ $user->avatar }}</td>
         <td>
-            <a href="" class="glyphicon glyphicon-pencil">Edit</a>
-            <a href="" class="glyphicon glyphicon-eye-open">View</a>
-            <a href="" class="glyphicon glyphicon-trash">Delete</a>
-            
-
+            <a href="" class="glyphicon glyphicon-pencil">{{ trans('label_trans.view')}}</a>
+            <a href="" class="glyphicon glyphicon-eye-open">{{ trans('label_trans.edit')}}</a>
+            <a href="" class="glyphicon glyphicon-trash">{{ trans('label_trans.delete')}}</a>
         </td>
     </tr>
     @endforeach
