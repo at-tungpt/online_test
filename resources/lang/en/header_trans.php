218a8e1398a585875b653666f51181f0faa4166b
at-tungpt <tungpt@asiantech.vn>
2017-01-18 10:54:44 +0700

    fixed feature#7

--- resources/lang/en/header_trans.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-    'online_test' => 'MANAGER ONLINE TEST ',
-    'test' => 'TEST',
-    'profile' => 'Profile',
-    'sign_out' => 'Log Out'
-    ];
--- resources/lang/en/label_trans.php
+++ resources/lang/en/label_trans.php
@@ -6,4 +6,8 @@ return [
     'not_found' => 'Not found',
     'old_password_does_not_match' => 'Old password does not match',
     'update_successfully' => 'Update successfully',
+    'online_test' => 'MANAGER ONLINE TEST ',
+    'test' => 'TEST',
+    'profile' => 'Profile',
+    'log_out' => 'Log Out'
 ];
