6b638bae7a80c3e01f1ec8a4630db03a8d8606f6
at-tungpt <tungpt@asiantech.vn>
2017-01-18 10:14:53 +0700

    Edit profile

---
+++ resources/lang/en/label_trans.php
@@ -0,0 +1,9 @@
+<?php
+return [
+
+    'save' => 'SAVE',
+    'back' => 'BACK',
+    'not_found' => 'Not found',
+    'old_password_does_not_match' => 'Old password does not match',
+    'update_successfully' => 'Update successfully',
+];
